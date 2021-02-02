ba02dee86988bf36feb77795eb6a0a8cfbcb1184
leasytime.nl <leasytime.nl@gmail.com>
2021-02-02 21:16:11 +0100

    Revert "init commit"
    This reverts commit 7eb9941306f00e7bdf75494aff39c6b3fc415d52.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
+
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -20,7 +23,70 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
-        name: 'Amber & Jeroen'
+        name: 'Jeroen Swart'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About us',
+        helpText: 'This is some helpful text',
+        name: 'Jeroen Swart'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help is underway',
+        name: 'Jeroen Swart'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    } 
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        } 
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData, 
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })  
+    }
+
+    res.send({
+        products: [
+            { games: ['Pokemon'] },
+            { cards: ['Poker']}
+        ]
+    })
+
+})
+
+app.get('/help/*', (req,res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found',
+        name: 'Jeroen'
     })
 })
 
@@ -28,7 +94,7 @@ app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
         errorMessage: 'Page not found',
-        name: 'Amber & Jeroen'
+        name: 'Jeroen'
     })
 })
 
